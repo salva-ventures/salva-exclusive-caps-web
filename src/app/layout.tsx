9db1573a827c2fcd57c197a1f966c38dfc65ec60
Carlos Sánchez Gutiérrez <A01412419@tec.mx>
2026-04-04 17:08:22 -0600

    Remove padding from main content area

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
     <html lang="es" className="bg-black">
       <body className="min-h-screen flex flex-col bg-black font-sans text-white">
         <Navbar />
-        <main className="flex-1 pt-20">{children}</main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
