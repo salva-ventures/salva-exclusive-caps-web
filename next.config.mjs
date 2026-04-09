95bbc92a8119902cf34a66b317fe3d33f83e7640
Carlos Sánchez Gutiérrez <A01412419@tec.mx>
2026-04-09 02:38:08 -0600

    Update remote image patterns in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
+        protocol: "https",
+        hostname: "qeuyppispekujnvabnto.supabase.co",
+        pathname: "/storage/v1/object/public/**",
       },
     ],
   },
